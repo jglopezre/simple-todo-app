0d1601b4f1e22835ea47cb495b171f7ddb3ff6e3
Javier Lopez <javierlopez_85@hotmail.com>
2023-03-04 20:48:25 -0300

    WarningPill Component fixed

--- src/components/WarningPill.jsx
+++ src/components/WarningPill.jsx
@@ -2,17 +2,17 @@ export const WarningPill = ({count}) => {
   const text = 'Tareas Pendientes';
   return (
     <>
-    {
-      count <= 0
-      ? ''
-      : count > 9
-        ? <div className="warning-pill">
-            {`${text}: +9`}
-          </div>
-        : <div className="warning-pill">
-            {`${text}: ${count}`}
-          </div>
-    }    
+      {
+        count <= 0
+        ? ''
+        : count > 9
+          ? <div className="warning-pill">
+              {`${text}: +9`}
+            </div>
+          : <div className="warning-pill">
+              {`${text}: ${count}`}
+            </div>
+      }    
     </>
     
   )
